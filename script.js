0151fac32452214bf297f45b9254f5b03122f3ae
Benil <beniljosenellad@gmail.com>
2022-03-26 14:38:58 +0530

    repo push commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let b = "name";
+
+function greet () {
+    let a = "hello";
+    console.log(a);
+
+    if(true)
+    {
+        let n = "benil";
+        var m = "jose";
+    }
+}
+
+greet();
+
+console.log(b);
+console.log(n);                                                                                   
+console.log(m);
